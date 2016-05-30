ce45d7719510da4f20117ef07523786be753f3dd
Mihir Patil <mihir.patil@berkeley.edu>
2016-05-29 17:49:52 -0700

    Minor changes

--- fair_comparison.py
+++ fair_comparison.py
@@ -6,11 +6,11 @@ import numpy as np
 import time
 
 # Setting up the input data
-num_frames = 50
-pfov_length = 100
+num_frames = 20
+pfov_length = 1000
 
 data_height = 100
-data_width = 10000
+data_width = 1000
 
 total_size = num_frames * data_height * data_width
 input_data = np.array(list(range(total_size))).reshape((num_frames, -1))
@@ -19,7 +19,7 @@ print "Input size: ", total_size
 
 
 # Setting up PyOP
-def dcRemovalPyop(block_set, height, length):
+def dcRemovalPyop(height, length):
     """
     :block_set: The block_set
     :height: The height of the data
@@ -32,12 +32,13 @@ def dcRemovalPyop(block_set, height, length):
         dc_values_rep = tile(dc_values, (length, 1)).T
         return (pfovs - dc_values_rep).reshape((height, -1))
 
-    return dcRem(block_set)
+    return dcRem
 
+dcRemPyop = dcRemovalPyop(data_height, pfov_length)
 
 # PyOP
 pyop_start_time = time.time()
-pyop_result = np.array([dcRemovalPyop(frame, data_height, pfov_length) for frame in input_data])
+pyop_result = np.array([dcRemPyop(frame) for frame in input_data])
 pyop_total_time = time.time() - pyop_start_time
 print "PyOP Time  : ", pyop_total_time
 
